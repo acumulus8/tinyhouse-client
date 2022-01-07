060fc426bbc0f6902a68ce947851aa8af77f536b
acmulus8 <timwilburn8@gmail.com>
2022-01-07 15:09:09 -0800

    feat: display listings and delete listings
    Now displays listings with the option to delete each one. Updated to use the useState hook, and refactored to handle re-rendering the updated list

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { server } from "../../lib/api";
-import { ListingsData, DeleteListingData, DeleteListingVariables } from "./types";
+import { ListingsData, DeleteListingData, DeleteListingVariables, Listing } from "./types";
 
 const LISTINGS = `
   query Listings {
@@ -30,27 +30,38 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
+  const [listings, setListings] = useState<Listing[] | null>(null);
+  
   const fetchListings = async () => {
     const { data } = await server.fetch<ListingsData>({ query: LISTINGS })
-    console.log(data);
-
+    setListings(data.listings);
   }
 
-  const deleteListing = async () => {
-    const { data } = await server.fetch<DeleteListingData, DeleteListingVariables>({ 
+  const deleteListing = async (id: string) => {
+    await server.fetch<DeleteListingData, DeleteListingVariables>({ 
       query: DELETE_LISTING,
-      variables: {
-        id: '61d86cf40d03b7d1487db8fd'
-      }
+      variables: { id }
     })
-    console.log(data);
+    fetchListings();
   }
+
+  const listingsList =
+    <ul>
+    {listings?.map((listing) => {
+      return <li key={listing.id}>
+        {listing.title}
+        <button onClick={() => deleteListing(listing.id)}>Delete!</button>
+        </li>
+    })}
+  </ul>
+
   
   return (
     <div>
       <h2>{title}</h2>
+      {listingsList}
       <button onClick={fetchListings}>Query Listings!</button>
-      <button onClick={deleteListing}>Delete Listings!</button>
+      
     </div>
   )
 }
