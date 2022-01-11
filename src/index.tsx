2a43ba15ad85803e8ebf6a2db18c853476ec9365
acmulus8 <timwilburn8@gmail.com>
2022-01-11 15:46:19 -0800

    feat - added react router package
    added all the routes and new folder/file structure. deleted old code. solved react-router v5 vs. v6 issues
    
    [BREAKING]: this eliminates old code from v1 of the course and introduces an updated folder/file structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { render } from "react-dom";
-import { Listings } from "./sections";
+import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
 import "./styles/index.css";
 
 const client = new ApolloClient({
@@ -9,8 +10,23 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/host" element={<Host />} />
+        <Route path="/listing/:id" element={<Listing />} />
+        <Route path="/listings/:location" element={<Listings />} />
+        <Route path="/user/:id" element={<User />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  )
+}
+
 render(
   <ApolloProvider client={client} >
-    <Listings title="TinyHouse Listings" />
+    <App />
   </ApolloProvider>
   , document.getElementById("root"));
