8cc639b2c3dfd49170dab5d5e57de076f30c4cd7
acmulus8 <timwilburn8@gmail.com>
2022-01-14 15:23:52 -0800

    feat - finished user authentication
    Added the cookie from the server, added a token to the request headers for any sensitive requests being made in the future. Added UI and function updates throughout to support the session persistance and logging in via the cookie.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { Layout, Affix } from "antd";
+import React, { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache, useMutation } from "@apollo/client";
+import { Layout, Affix, Spin } from "antd";
 import { Home, Host, Listing, Listings, NotFound, User, LogIn, AppHeader } from "./sections";
+import { AppHeaderSkeleton, ErrorBanner } from "./lib/components";
 import "./styles/index.css";
 import { Viewer } from "./lib/types";
+import { LOG_IN } from "./lib/graphql/mutations";
+import { LogIn as LogInData, LogInVariables } from "./lib/graphql/mutations/LogIn/__generated__/LogIn";
+
+const token = sessionStorage.getItem("token");
 
 const client = new ApolloClient({
 	uri: "/api",
 	cache: new InMemoryCache(),
+	headers: {
+		"X-CSRF-TOKEN": token || "",
+	},
+	// request: async (operation) => {
+	// 	const token = sessionStorage.getItem("token");
+	// 	operation.setContext({
+	// 		headers: {
+	// 			"X-CSRF-TOKEN": token,
+	// 		},
+	// 	});
+	// },
 });
 
 const initialViewer: Viewer = {
@@ -22,6 +38,40 @@ const initialViewer: Viewer = {
 
 const App = () => {
 	const [viewer, setViewer] = useState<Viewer>(initialViewer);
+	const [logIn, { error }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+		onCompleted: (data) => {
+			if (data && data.logIn) {
+				setViewer(data.logIn);
+
+				if (data.logIn.token) {
+					sessionStorage.setItem("token", data.logIn.token);
+				} else {
+					sessionStorage.removeItem("token");
+				}
+			}
+		},
+	});
+
+	const logInRef = useRef(logIn);
+
+	useEffect(() => {
+		logInRef.current();
+	}, []);
+
+	if (!viewer.didRequest && !error) {
+		return (
+			<Layout className="app-skeleton">
+				<AppHeaderSkeleton />
+				<div className="app-skeleton__spin-section">
+					<Spin size="large" tip="Launching Tinyhouse" />
+				</div>
+			</Layout>
+		);
+	}
+
+	const logInErrorBannerElement = error ? (
+		<ErrorBanner description="We weren't able to verify if you were logged in. Please try again later" />
+	) : null;
 
 	return (
 		<Router>
@@ -29,6 +79,7 @@ const App = () => {
 				<Affix offsetTop={0}>
 					<AppHeader viewer={viewer} setViewer={setViewer} />
 				</Affix>
+				{logInErrorBannerElement}
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/host" element={<Host />} />
--- src/sections/Login/index.tsx
+++ src/sections/Login/index.tsx
@@ -8,91 +8,87 @@ import { AuthUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__gene
 import { LogIn as LogInData, LogInVariables } from "../../lib/graphql/mutations/LogIn/__generated__/LogIn";
 import googleLogo from "./assets/google_logo.png";
 import { Viewer } from "../../lib/types";
-import { ErrorBanner } from "../../lib/components/ErrorBanner";
+import { ErrorBanner } from "../../lib/components";
 import { displayErrorMessage, displaySuccessNotification } from "../../lib/utils";
 
 const { Content } = Layout;
 const { Text, Title } = Typography;
 
 interface Props {
-  setViewer: (viewer: Viewer) => void;
+	setViewer: (viewer: Viewer) => void;
 }
 
 export const LogIn = ({ setViewer }: Props) => {
-  const client = useApolloClient();
-  const [
-    logIn, 
-    { data: logInData, loading: logInLoading, error: logInError }
-  ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
-    onCompleted: (data) => {
-      if (data && data.logIn) {
-        setViewer(data.logIn);
-        displaySuccessNotification("You've successfully logged in");
-      }
-    }
-  });
+	const client = useApolloClient();
+	const [logIn, { data: logInData, loading: logInLoading, error: logInError }] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+		onCompleted: (data) => {
+			if (data && data.logIn && data.logIn.token) {
+				setViewer(data.logIn);
+				sessionStorage.setItem("token", data.logIn.token);
+				displaySuccessNotification("You've successfully logged in");
+			}
+		},
+	});
 
-  const logInRef = useRef(logIn);
-  
-  useEffect(() => {
-    const code = new URL(window.location.href).searchParams.get("code");
+	const logInRef = useRef(logIn);
 
-    if (code) {
-      logInRef.current({ variables: { input: { code } } })
-    }
-  }, [])
-  
-  const handleAuthorize = async () => {
-    try{ 
-      const { data } = await client.query<AuthUrlData>({
-        query: AUTH_URL
-      })
-      window.location.href = data.authUrl
-    } catch (error) {
-      displayErrorMessage("Sorry! We weren't able to log you in. Please try again later!");
-    }
-  }
+	useEffect(() => {
+		const code = new URL(window.location.href).searchParams.get("code");
 
-  if (logInLoading) {
-    return (
-      <Content className="log-in">
-        <Spin size="large" tip="Logging you in..." />
-      </Content>
-    )
-  }
+		if (code) {
+			logInRef.current({ variables: { input: { code } } });
+		}
+	}, []);
 
-  if (logInData && logInData.logIn) {
-    const { id: viewerId } = logInData.logIn;
-    return <Navigate to={`/user/${viewerId}`} />
-  }
+	const handleAuthorize = async () => {
+		try {
+			const { data } = await client.query<AuthUrlData>({
+				query: AUTH_URL,
+			});
+			window.location.href = data.authUrl;
+		} catch (error) {
+			displayErrorMessage("Sorry! We weren't able to log you in. Please try again later!");
+		}
+	};
 
-  const logInErrorBannerElement = logInError ? (
-    <ErrorBanner description="Sorry! We weren't able to log you in. Please try again later." />
-  ) : null;
+	if (logInLoading) {
+		return (
+			<Content className="log-in">
+				<Spin size="large" tip="Logging you in..." />
+			</Content>
+		);
+	}
 
+	if (logInData && logInData.logIn) {
+		const { id: viewerId } = logInData.logIn;
+		return <Navigate to={`/user/${viewerId}`} />;
+	}
 
-  
-  return (
-    <Content className="log-in">
-      {logInErrorBannerElement}
-      <Card className="log-in-card">
-        <div className="ton-in-card__intro">
-          <Title level={3} className="log-in-card__intro-title">
-            <span role="img" aria-label="wave">
-              👋
-            </span>
-          </Title>
-          <Title level={3} className="log-in-card__intro-title">
-            Log in to TinyHouse!
-          </Title>
-          <Text>Sign in with Google to start booking available rentals!</Text>
-        </div>
-        <button className="log-in-card__google-button" onClick={handleAuthorize}>
-          <img src={googleLogo} alt="Google logo" className="log-in-card__google-button-logo" />
-          <span className="log-in-card__google-button-text">Sign in with Google</span>
-        </button>
-        <Text type="secondary">Note: By signing in, you'll be redirected to the Google consent form to sign in with your google account.</Text>
-      </Card>
-    </Content>
-  )
-}
+	const logInErrorBannerElement = logInError ? <ErrorBanner description="Sorry! We weren't able to log you in. Please try again later." /> : null;
+
+	return (
+		<Content className="log-in">
+			{logInErrorBannerElement}
+			<Card className="log-in-card">
+				<div className="ton-in-card__intro">
+					<Title level={3} className="log-in-card__intro-title">
+						<span role="img" aria-label="wave">
+							👋
+						</span>
+					</Title>
+					<Title level={3} className="log-in-card__intro-title">
+						Log in to TinyHouse!
+					</Title>
+					<Text>Sign in with Google to start booking available rentals!</Text>
+				</div>
+				<button className="log-in-card__google-button" onClick={handleAuthorize}>
+					<img src={googleLogo} alt="Google logo" className="log-in-card__google-button-logo" />
+					<span className="log-in-card__google-button-text">Sign in with Google</span>
+				</button>
+				<Text type="secondary">
+					Note: By signing in, you'll be redirected to the Google consent form to sign in with your google account.
+				</Text>
+			</Card>
+		</Content>
+	);
+};
